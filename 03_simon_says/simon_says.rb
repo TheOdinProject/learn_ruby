93e6c3bd09578f228b8556b9cf9e15f4be172211
JGilly <jlgillan@comcast.net>
2016-08-05 16:11:40 -0600

    Finished exercises 00-06

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,35 @@
 #write your code here
+def echo(what)
+	what	
+end
+
+def shout(what)
+	what.upcase
+end
+
+def repeat(what, num = 2) # equal sign makes it an optional param with a default value
+	array = Array.new(num, what)
+	array.join(' ')
+end
+
+def start_of_word(word, numChars = 1)
+	word[0..numChars-1]
+end
+
+def first_word(someString)
+	array = someString.split
+	array[0]
+end
+
+def titleize(someString)
+	array = someString.split
+	array[0] = array[0].capitalize
+	array.map! { |e| 
+		if e == "and" or e == "or" or e == "to" or e == "the"
+			e #do nothing
+		else
+			e.capitalize 
+		end
+	}
+	array.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
-#write your code here
+def translate(someString)
+	words = someString.split
+	pigLatin = []
+
+	words.each do |word|
+		firstLetter = word[0].downcase
+
+		case firstLetter
+		when 'a','e','i','o','u'
+			pigLatin.push(word + 'ay')
+		else
+			word.gsub!("qu", '*')
+			index = word.downcase.index(/[aeiou]/)
+			firstPart = word[0..index-1]
+			word = ( word[index..-1]+firstPart+"ay" )
+			word.gsub!('*', "qu") 
+			pigLatin.push(word)
+		end
+	end
+	
+	pigLatin.join(" ")
+end
