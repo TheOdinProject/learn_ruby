9612f43cd1461628c56989b3c63cfcf33ed84157
Kevin <kphyde@gmail.com>
2016-10-20 15:25:14 -0700

    initial commit and finished exercises

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+
+def echo(word)
+  word
+end
+
+def shout(word)
+  word.upcase
+end
+
+def repeat(word,x=2)
+  str = "#{word} #{word}"
+  if x > 2
+    str << (" #{word}") * (x - 2)
+    str
+  else
+    str
+  end
+end
+
+def start_of_word(word,letters)
+  word[0,(letters.to_i)]
+end
+
+def first_word(string)
+  string.split(" ").first
+end
+
+def titleize(title)
+  small_words = %w(a of the and over)
+  string = title.downcase.split(" ")
+  fixed_title = []
+
+  string.each_with_index do |word, index|
+    if index == 0 || !small_words.include?(word)
+      fixed_title << word.capitalize
+    else
+      fixed_title << word
+    end
+  end
+  return fixed_title.join(" ")
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def initialize(title=nil)
+    @title = title && title.capitalize!
+  end
+
+  def title=(new_title)
+    array = new_title.split(" ")
+    array.each_with_index do |word, i|
+      if i == 0
+        word.capitalize!
+      else
+        unless ['a', 'the', 'on', 'and', 'over', 'of', 'for', 'through', 'in', 'an'].include?(word)
+          word.capitalize!
+        end
+      end
+    end
+    @title = array.join(" ")
+  end
+
 end
+
+#
+# @book = Book.new
+# @book.title = "inferno"
