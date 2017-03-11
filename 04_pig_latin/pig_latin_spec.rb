de17ff85e4d775749b6a4a3f7cf26fcfd7e6f988
Boscardin <boscardinlondon@gmail.com>
2017-03-11 01:21:55 +0000

    My solutions

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -1,68 +1,49 @@
-# # Topics
-#
-# * modules
-# * strings
-#
-# # Pig Latin
-#
-# Pig Latin is a made-up children's language that's intended to be confusing. It obeys a few simple rules (below) but when it's spoken quickly it's really difficult for non-children (and non-native speakers) to understand.
-#
-# Rule 1: If a word begins with a vowel sound, add an "ay" sound to the end of the word.
-#
-# Rule 2: If a word begins with a consonant sound, move it to the end of the word, and then add an "ay" sound to the end of the word.
-#
-# (There are a few more rules for edge cases, and there are regional variants too, but that should be enough to understand the tests.)
-#
-# See <http://en.wikipedia.org/wiki/Pig_latin> for more details.
-#
-#
-
 require "pig_latin"
 
 describe "#translate" do
 
   it "translates a word beginning with a vowel" do
     s = translate("apple")
-    expect(s).to eq("appleay")
+    s.should == "appleay"
   end
 
   it "translates a word beginning with a consonant" do
     s = translate("banana")
-    expect(s).to eq("ananabay")
+    s.should == "ananabay"
   end
 
   it "translates a word beginning with two consonants" do
     s = translate("cherry")
-    expect(s).to eq("errychay")
+    s.should == "errychay"
   end
 
   it "translates two words" do
     s = translate("eat pie")
-    expect(s).to eq("eatay iepay")
+    s.should == "eatay iepay"
   end
 
   it "translates a word beginning with three consonants" do
-    expect(translate("three")).to eq("eethray")
+    translate("three").should == "eethray"
   end
 
   it "counts 'sch' as a single phoneme" do
     s = translate("school")
-    expect(s).to eq("oolschay")
+    s.should == "oolschay"
   end
 
   it "counts 'qu' as a single phoneme" do
     s = translate("quiet")
-    expect(s).to eq("ietquay")
+    s.should == "ietquay"
   end
 
   it "counts 'qu' as a consonant even when it's preceded by a consonant" do
     s = translate("square")
-    expect(s).to eq("aresquay")
+    s.should == "aresquay"
   end
 
   it "translates many words" do
     s = translate("the quick brown fox")
-    expect(s).to eq("ethay ickquay ownbray oxfay")
+    s.should == "ethay ickquay ownbray oxfay"
   end
 
   # Test-driving bonus:
@@ -70,3 +51,5 @@ describe "#translate" do
   # * retain the punctuation from the original phrase
 
 end
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
