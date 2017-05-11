c7722c141ed21546619e6b4a75a352a9d6f5f748
Ray Climie <seederwood@gmail.com>
2017-05-11 13:18:17 -0300

    finish (entire) calculator problem

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
 #write your code here
+def add(a, b)
+  return a + b
+
+end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum (array)
+  
+  array.inject(0) {|sum, a| sum += a}
+end
+# extra Credit
+
+def multiply (a, *b)
+  return b.reduce(a) {|o, x| o * x}
+end
+
+def power(a, b)
+  return a ** b
+end
+
+def factorial(n)
+  if n == 0
+     return 1
+  else
+     return n * factorial(n-1)
+  end
+end
