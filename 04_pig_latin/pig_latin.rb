fee4aba8e7e9d5e76f37f9e6e9f390646c321974
Ray Climie <seederwood@gmail.com>
2017-05-21 00:46:07 -0300

    refactor and finish pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,28 +1,36 @@
-def translate(input)
-  if input.count(' ') < 1
-    translator(input)
-  else
-    words = input.split(' ')
-    words.map! { |text| translator(text) }.join(' ')
+def vowel?(str)
+  %w[a e i o u].include?(str)
+end
 
+def q?(str)
+  %w[q].include?(str)
+end
+
+def translate_word(word)
+  q_index = word.chars.find_index { |c| q?(c) }
+  if !q_index.nil?
+    if q_index === 0
+      leading_consonants = word[0..1]
+      rest_of_word = word[2..-1]
+      rest_of_word + leading_consonants + 'ay'
+    else
+      leading_consonants = word[0..q_index + 1]
+      rest_of_word = word[q_index + 2..-1]
+      rest_of_word + leading_consonants + 'ay'
+    end
+  else
+    first_vowel_index = word.chars.find_index { |c| vowel?(c) }
+    if first_vowel_index.zero?
+      word + 'ay'
+    else
+      leading_consonants = word[0..first_vowel_index - 1]
+      rest_of_word = word[first_vowel_index..-1]
+      rest_of_word + leading_consonants + 'ay'
+    end
   end
 end
 
-def translator(text)
-  if text.start_with?('a', 'e', 'i', 'o', 'u')
-    text << 'ay'
-  else
-    pos = nil
-    %w[a e i o u].each do |vowel|
-      pos = text.index(vowel)
-      break unless pos.nil?
-    end
-    if !pos.nil?
-      pre = text.partition(text[pos, 1]).first
-      text.slice!(pre)
-      text << pre + 'ay'
-    else
-      text << 'ay'
-    end
-  end
+def translate(sentence)
+  words = sentence.split(' ')
+  words.map { |w| translate_word(w) }.join(' ')
 end
