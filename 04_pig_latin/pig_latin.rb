be1415455eebfb8ac0a795a650d42ac186919723
Ray Climie <seederwood@gmail.com>
2017-05-21 00:11:17 -0300

    three consonant pig latin translation

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,28 @@
-def translate(text)
-  array = text.split('')
-  if array[0] =~ /[aeiou]/
-    return array.join('').to_s + 'ay'
+def translate(input)
+  if input.count(' ') < 1
+    translator(input)
   else
-    return array.push(array.shift).join('').to_s + 'ay'
+    words = input.split(' ')
+    words.map! { |text| translator(text) }.join(' ')
+
+  end
+end
+
+def translator(text)
+  if text.start_with?('a', 'e', 'i', 'o', 'u')
+    text << 'ay'
+  else
+    pos = nil
+    %w[a e i o u].each do |vowel|
+      pos = text.index(vowel)
+      break unless pos.nil?
+    end
+    if !pos.nil?
+      pre = text.partition(text[pos, 1]).first
+      text.slice!(pre)
+      text << pre + 'ay'
+    else
+      text << 'ay'
+    end
   end
 end
