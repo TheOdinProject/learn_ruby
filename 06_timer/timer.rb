5321cf6f6e3f3dbfd5ec41728eff97dd7f2cd2be
Ray Climie <seederwood@gmail.com>
2017-05-22 23:09:58 -0300

    finished Timer project

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  # write your code here
+  attr_reader :seconds
+  attr_writer :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+  end
+
+  def time_string
+    secs = @seconds
+    mins, secs = secs.divmod(60)
+    hours, mins = mins.divmod(60)
+
+    [hours, mins, secs].map { |e| e.to_s.rjust(2, '0') }.join ':'
+  end
 end
