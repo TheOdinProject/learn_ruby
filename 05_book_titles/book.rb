e32bd2f891da8be3632c782223b884ddf059e46d
Peter J. Bates <peterbates8@gmail.com>
2017-06-16 13:05:19 -0500

    Solutions to 04, 05, 06

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+	attr_reader :title
+
+	def title=(name)
+    except = ["and", "do", "the", "a", "an", "in", "of"]
+		name = name.split(" ")
+
+		name.each do |word|
+			if !except.include? word
+				word.capitalize!
+			end
+		end
+
+		if name.first == "the"
+			name.first.capitalize!
+		end
+
+		@title = name.join(" ")
+
+	end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    number = @seconds
+
+    if number == 0
+      "00:00:00"
+    else
+      hour = (number/3600).floor
+      minute = ((number - (hour*3600))/60).floor
+      second = number - (hour*3600) - (minute*60)
+
+      pad(hour) + ':' + pad(minute) + ':' + pad(second)
+
+    end
+  end
+  def pad(number)
+    if number.to_s.length == 1
+      '0' + number.to_s
+    else
+      number.to_s
+    end
+  end
 end
