1b72dc42434082f1b2307773de5d1b32c09f4bab
Bartosz Dryl <abraxaswisz@gmail.com>
2017-09-05 14:38:45 +0200

    trying with specs

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,3 +6,22 @@ end
 def subtract(a,b)
 	a - b
 end
+
+def sum(array)
+	n = array.length - 1
+	sum = array[n]
+	if sum == nil
+		sum = 0
+	else
+		while n != 0
+			sum = sum + array[n-1]
+			n = n - 1
+		end
+	end
+	sum
+end
+
+
+def multiply(a,b)
+	a * b
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,10 +79,13 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+    expect(multiply(3,5)).to eq(15)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+    expect(multiply([3,5,2])).to eq(30)
+  end
 end
 
 describe "#power" do
