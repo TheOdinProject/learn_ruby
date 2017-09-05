fb3eebbcaa2da40aad5c2412d36d7d90e95d8360
Bartosz Dryl <abraxaswisz@gmail.com>
2017-09-05 22:53:19 +0200

    finished calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -40,6 +40,33 @@ def power(a,b)
 	a**b
 end
 
-def factorial #uzyj tablic - 
 
+
+
+
+def factorial(a)
+	if a == 0 || a == nil
+		a = 1 
+	else
+		emptyarray = []
+		emptyarray << a
+		a = a+1
+		while a != 1
+			emptyarray << a-1
+			a = a-1
+		end
+		n = emptyarray.length - 1
+		a = emptyarray[n]
+		if a == nil 
+			a = 0 
+		else
+			while n !=1 
+				a = a * emptyarray[n-1]
+				n = n -1
+			end
+		end
+	end
+	a
 end
+
+puts factorial(10)
