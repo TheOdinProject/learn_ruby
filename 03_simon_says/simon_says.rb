7a6cf0b8cc54f93935a3cf9df11ba1773dcd0fa6
Bartosz Dryl <abraxaswisz@gmail.com>
2017-09-06 01:53:29 +0200

    working on start_of_word

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+	def initialize
+	end
+	def echo(a)
+		echo = a
+	end
+
+	def shout(a)
+		shout = a.upcase
+	end
+
+
+	def repeat(a,b=2)
+		repeat = ("#{a} " * b).chop
+	end
+
+	def start_of_word(a,b)
+		if b == 1
+		start_of_word = a[b-1]
+		else
+		start_of_word = ""
+		while  b != 1
+		b = b-1
+		start_of_word = a[b-1]
+		b.times do 
+		start_of_word = start_of_word + a[b]
+		end
+		start_of_word
+	end
+	end
