deed520b98b9b9a9e2df144a8fafc817832702cc
Bartosz Dryl <abraxaswisz@gmail.com>
2017-10-06 02:23:33 +0200

    all tests passed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -5,7 +5,26 @@ class Book
 # write your code here	
 	attr_accessor :title
 	def title
-		@title.to_s.capitalize
+
+		@title = @title.to_s.capitalize
+		nocapitalize = ["and","over","the","in","of", "a", "an"]
+		if @title.include?(" ")
+
+			splitTitle = @title
+			splitTitle = splitTitle.split.map! do |name| 
+				if nocapitalize.include?(name)  #przeanalizuj sobie i zwroc uwage na składnie!
+				name
+				else
+				name.capitalize
+				end
+
+		end
+
+			splitTitle[0].capitalize!
+			splitTitle = splitTitle.join(" ")
+			@title = splitTitle
+		end
+		@title 
 	end
 	# title
 end
