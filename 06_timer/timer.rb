1725fa988639f55e7cb34eec10227378cfe313e0
Bartosz Dryl <abraxaswisz@gmail.com>
2017-10-08 16:05:59 +0200

    done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :padded # tu już stworzyłem metode seconds 
+def initialize # tu stworzylem domyślną wartość dla seconds - która równa się 0
+	self.seconds = 0 # musze użyć self!! 
 end
+
+  def time_string
+
+    s = seconds
+    if s / 60 > 0 
+      s = s % 60
+    end
+
+    m = seconds / 60
+    if m / 60 > 0
+      m = m % 60
+    end
+
+    h = seconds / 3600
+
+time_string = padded(h) + ":" + padded(m) + ":" + padded(s)
+
+  end
+
+  def padded n
+    if n < 10
+    n = "0" + n.to_s
+    else
+      n = n.to_s
+    end
+  end
+
+
+end
+@timer = Timer.new
+@timer.time_string = 00 
