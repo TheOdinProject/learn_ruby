379c17432766e75f6c4748a18e04c8224119c383
PhatKiwi <ethanandy90@gmail.com>
2017-10-12 15:22:21 -0600

    finished piglatin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,16 @@
-#write your code here
+# write your code here
+def translate(string)
+  vowels = %w[a e i o u]
+  words = string.split.map do |word|
+    if vowels.include?(word[0])
+      word + 'ay'
+    elsif word.include?('qu')
+      idx = word.index(/[aeio]/)
+      word[idx, word.length] + word[0, idx] + 'ay'
+    else
+      idx = word.index(/[aeiou]/)
+      word[idx, word.length] + word[0, idx] + 'ay'
+    end
+  end
+  words.join(' ')
+end
