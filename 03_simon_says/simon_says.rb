01c06762f1ca083535b6002da7b7c87d532d2a5a
PhatKiwi <ethanandy90@gmail.com>
2017-10-12 14:53:01 -0600

    finished simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+# write your code here
+def echo(say)
+  say.chomp
+end
+
+def shout(words)
+  words.upcase
+end
+
+def repeat(word, times = 2)
+  ([word] * times).join(' ')
+end
+
+def start_of_word(word, num)
+  word[0...num]
+end
+
+def first_word(string)
+  string.split(' ').first
+end
+
+def titleize(string)
+  words = string.split.map do |word|
+    if %w[the and over].include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(' ')
+end
