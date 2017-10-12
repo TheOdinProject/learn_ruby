4ee34dc2179665fac49f14a863c4a73d71de2ff6
PhatKiwi <ethanandy90@gmail.com>
2017-10-12 14:22:26 -0600

    full calculator finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
-#write your code here
+# write your code here
+def add(i, j)
+  i + j
+end
+
+def subtract(i, j)
+  i - j
+end
+
+def sum(array)
+  number = 0
+  array.each { |i| number += i }
+  number
+end
+
+def multiply(array)
+  total = 1
+  array.each { |i| total *= i }
+  total
+end
+
+def power(a, b)
+  a**b
+end
+
+def factorial(x)
+  total = 1
+  (1..x).each { |i| total *= i } unless x <= 1
+  total
+end
