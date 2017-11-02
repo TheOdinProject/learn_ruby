f2a7533cc6dd5fa49b70dbd84887b00f623485a5
Brandon Veth <veth91@gmail.com>
2017-11-01 20:21:50 -0400

    solved

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add (input1,input2)
+  input1 + input2
+end
+
+def subtract (input1, input2)
+  input1 - input2
+end
+
+def sum (array)
+if array == []
+  0
+elsif array.length == 1
+  array[0]
+else
+  sum = 0
+  i = 0
+  while i < array.length
+    sum += array[i]
+    i += 1
+  end
+sum
+end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,41 @@
 #write your code here
+def echo (input)
+  input
+end
+
+def shout (input)
+  input.upcase
+end
+
+def repeat (input, arg2="2")
+  input = input + " "
+  output = input*arg2.to_i
+  output.chop
+end
+
+def start_of_word (input, position)
+  n = position-1
+  ouput = input[0..n]
+end
+
+def first_word (input)
+  output = input.split(" ")
+  output[0]
+end
+
+def titleize (input)
+  array = input.split(" ")
+  i = 0
+  string = ()
+  while i <array.length #2
+    if array[i] != "and" && array[i] != "the" && array[i] != "over"
+      array[i] = array[i].capitalize
+      output = array.join(" ")
+      i += 1
+    else
+    i += 1
+    end
+  end
+output[0] = output[0].capitalize
+output
+end
