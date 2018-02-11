ed291138d78ec8cd8028f9ee8091c94a66e1da31
RGu0000 <RDInquire>
2018-02-11 12:46:10 +0100

    Example 02 done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add (num1, num2)
+  num1+num2
+ end
+
+def subtract(num1, num2)
+  num1-num2
+end
+
+def sum(array)
+  sum = 0
+  array.each { |a| sum+=a }
+  sum
+end
+
+def power(base, exponent)
+  base**exponent
+end
+
+def multiply(*nums)
+  total=1
+  nums.each{|n| total*=n}
+  total
+end
+
+def factorial(num)
+  if num==0
+    1
+  else
+    num*factorial(num-1)
+  end
+end
