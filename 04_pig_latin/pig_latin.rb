d907526d43e9d374f004eea56778bb66aade11b7
Sava <vuckovicsava11@gmail.com>
2018-03-02 11:11:57 +0100

    03_pyg_latin in progress

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+
+def translate(input)
+  result = []
+  input.split(/\s/).each do |word|
+    match = word.match(/^[^aeiou|qu]+/)
+    if match
+      extracted = match[0]
+      result << "#{word.gsub(extracted, '')}#{extracted}ay"
+    else
+      result << "#{word}ay"
+    end
+  end
+
+  result.join(" ")
+end
