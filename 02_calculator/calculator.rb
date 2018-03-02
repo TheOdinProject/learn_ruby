d1d00f852b7bb49df78b6442fbcad2e964f24de0
Sava <vuckovicsava11@gmail.com>
2018-03-02 10:11:46 +0100

    02_calculator completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(arr)
+  arr.inject(0) do |total, current|
+    total + current
+  end
+end
+
+def multiply(arr)
+  arr.inject(1) do |total, current|
+    total * current
+  end
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(n)
+  total = n > 0 ? 1 : 0
+
+  i = 1
+  n.times do
+    total *= i
+    i += 1
+  end
+
+  total
+end
