415c01b5c0ee7bc16fd6792c6ce3933cb5f726f7
Kevin Wahome <wahomekevin@gmail.com>
2018-03-05 10:28:52 +0300

    Added pig latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,11 +2,14 @@
 def translate(input)
   result = []
   input.split(/\s/).each do |word|
-    match = word.match(/^[^aeiou|qu]+/)
+    consonant_test = /^(qu|[^aeiou])+/
+    match = word.match(consonant_test)
     if match
+      # handle words that begin with consonants
       extracted = match[0]
       result << "#{word.gsub(extracted, '')}#{extracted}ay"
     else
+      # handle words tht begin with vowels
       result << "#{word}ay"
     end
   end
