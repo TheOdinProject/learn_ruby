044d77d35448def16a38d362fdbcf4a2871c1e08
WormCrew <44246461+WormCrew@users.noreply.github.com>
2018-11-08 19:01:54 +0000

    first solutions

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(a,b)
+    a+b
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(a)
+    a.reduce(0) {|b,c| b + c}
+end
+
+def multiply(a)
+    a.reduce() {|b,c| b * c}
+end
+
+def power(a,b)
+    a ** b
+end
+    
+def factorial(a)
+    (1..a).inject(:*)||1
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
-#write your code here
+def echo(word)
+    word
+end
+
+def shout(word)
+    word.upcase
+end
+
+def repeat(word,a=2)
+    ([word]*a).join(' ')
+end
+
+def start_of_word(word,a=1)
+    word[0,a]
+end
+
+def first_word(word)
+    word.split.first
+end
+
+def titleize(sentence)
+    sentence.capitalize
+    no_cap = ["and", "or", "over", "to", "the", "a", "but"]
+    sentence.split(" ").map { |word| no_cap.include?(word) ? word : word.capitalize }
+    
+end
