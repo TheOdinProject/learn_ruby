2385618c97aa5a19d030ac55527c3574d381ace0
Medrit <medritkaimi@gmail.com>
2019-01-18 16:09:50 +0100

    05 done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
+LOWERCASE = ['and', 'over', 'the', 'in', 'of', 'a', 'an']
+	def titleize(w)
+		t = w.split.map { |x| 
+		LOWERCASE.include?(x) ? x : x.capitalize }
+		t.first.capitalize!
+		t.join(' ')
+	end
+
 class Book
 # write your code here
+    def title=(t)
+        @title = titleize(t)
+    end
+    
+    def title
+        @title
+    end
 end
