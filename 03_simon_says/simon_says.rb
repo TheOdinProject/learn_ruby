1aea84626afbd790c96ad9331c23fb4ccba5f8c2
Medrit <medritkaimi@gmail.com>
2019-01-18 16:08:39 +0100

    03 done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+LOWERCASE = ['and', 'over', 'the']
+def  echo smth
+    smth
+end
+
+def shout smth
+    smth.upcase
+end
+
+def repeat smth, n = 2
+    ([smth] * n).join(" ")
+end
+
+def start_of_word smth, x
+    smth[0...x]
+end
+
+def first_word smth
+    smth.split.first
+end
+
+def titleize (w)
+    t = w.split.map { |x| 
+	LOWERCASE.include?(x) ? x : x.capitalize }
+	t.first.capitalize!
+	t.join(' ')
+
+    
+end
