839438c8b8b45210fa9ae61f744cc7163761bb9a
Medrit <medritkaimi@gmail.com>
2019-01-18 16:09:12 +0100

    02 done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add (a, b)
+    return a + b
+end
+
+def subtract (a, b)
+    return a - b
+end
+
+def sum (array)
+    result = 0
+    array.each do |item|
+        result = result + item
+    end
+    return result
+end
