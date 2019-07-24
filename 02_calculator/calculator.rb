61f546f611a9c7b6a8a3a523d3e445a946f4e569
gadget2013 <niccolo1469@tutanota.com>
2019-07-23 21:03:27 -0700

    learn_ruby modifications. July 23, 2019/ Tuesday / 21:03

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,24 @@
 #write your code here
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def sum(numbers)
+    numbers.inject(0){|total, number| total+number}
+end
+
+def multiply(*args)
+    args.reduce(:*)
+end
+
+def factorial x
+    if x <= 1
+        1
+    else
+        x * factorial(x-1)
+    end
+end
