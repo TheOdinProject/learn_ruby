f988256bcb9b80f9b8acdeff0a7d27efd82c7ca5
Arjuna Anday <asa2182@columbia.edu>
2019-09-22 22:36:23 -0500

    practice probs

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
 #write your code here
+def echo(word)
+    return word
+end
+
+def shout(word)
+    word.upcase
+end
+
+def repeat(word, num=2)
+    arr = []
+    num.times {arr << word}
+    arr.join(" ")
+end
+
+def start_of_word(word, num)
+    word[0...num]
+end
+
+def first_word(str)
+    arr = str.split(" ")
+    arr[0]
+end
+
+def titleize (str)
+
+    arr = str.capitalize.split(" ")
+
+
+
+    arr.each do |word|
+        if word != "the" && word != "and" && word != "over"
+            word.capitalize!
+        end
+    end
+    arr.join(" ")
+end
