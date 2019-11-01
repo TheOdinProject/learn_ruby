181c12a1693e11bd69d68eac97fe9d8da091197e
StewartJake <jacobstewart1987@gmail.com>
2019-11-01 08:52:49 -0700

    still working on repeat

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,25 @@
 #write your code here
+def echo phrase
+    phrase
+end
+
+def shout phrase
+    phrase.upcase
+end
+
+#def repeat phrase
+#    phrase + ' ' + phrase
+#end
+
+def repeat phrase, *times
+    if times.empty?
+        count = 2
+        #return phrase + ' ' + phrase
+    else
+        count = times[0]
+    end
+    for i in 0...count
+        builder.push(phrase)
+    end
+    builder.join(' ')
+end
