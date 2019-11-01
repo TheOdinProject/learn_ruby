5c2c5ab86acea87ca8a086d0fdd1957f78b2ce5a
StewartJake <jacobstewart1987@gmail.com>
2019-10-31 20:40:19 -0700

    ctof & ftoc fns

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc(temperature)
+    celsius = temperature - 32
+    celsius = celsius / 1.8
+    celsius.round
+end
+
+def ctof(temperature)
+    fahrenheit = temperature * 1.8
+    fahrenheit = fahrenheit + 32
+    fahrenheit.round(1)
+end
