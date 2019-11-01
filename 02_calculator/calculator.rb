432e152fccd6da5e484d410ab2715705f4055cd2
StewartJake <jacobstewart1987@gmail.com>
2019-10-31 21:25:49 -0700

    working on all fns

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+
+def add a, b
+    a + b
+end
+
+def subtract a, b
+    a - b
+end
+
+def sum numbers 
+    #just read documentation
+    numbers.sum
+end
+
+def multiply numbers 
+    a * b
+end
+
+def power a, b
+    a ** b
+end
+
+def factorial n
+    product = 0
+    for i in 0...n
+        product = product * i
+    end
+end 
