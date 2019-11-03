408e9043c32ad4c58fdff56700abe4cbd133da3e
StewartJake <jacobstewart1987@gmail.com>
2019-11-03 13:36:48 -0800

    100%

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
 class Timer
   #write your code here
+    attr_accessor :seconds
+
+    def initialize
+        @seconds = 0
+    end
+
+    def time_string
+        seconds = format @seconds % 60
+        minutes = format (@seconds / 60) % 60
+        hours   = format (@seconds / 3600) % 60
+        hours + ':' + minutes + ':' + seconds
+    end
+
+    def format time
+        time.to_s.rjust(2, '0')
+    end
+    
 end
