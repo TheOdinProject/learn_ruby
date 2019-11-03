f2983e400878ced51121a03eeba48448a466c0c3
StewartJake <jacobstewart1987@gmail.com>
2019-11-03 13:25:12 -0800

    100%

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+    attr_reader :title
+    def title=(title)
+        little_words = ["and", "of", 'a', "the", "over", "in","an"]
+        output = []
+        title = title.split(' ')
+        title.each_with_index do |word, index| 
+            if index == 0 || little_words.index(word) == nil
+                output.push word.capitalize
+            else
+                output.push word
+            end
+        end
+        @title = output.join(' ')
+    end
 end
