37780fd7998db6bc3c09f3182e0ff00229a1b873
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-05 14:05:48 -0800

    added solutions to 00, 01, and 03

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(number)
+    finally = (number - 32) * (5 / 9.0)
+    return finally
+end
+
+def ctof(number)
+    finally = (9.0 / 5) * number + 32
+    return finally
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,53 @@
-#write your code here
+def echo(phrase)
+    p phrase 
+
+end  
+
+
+def shout(phrase)
+    p phrase.upcase
+
+end
+
+def repeat(phrase, num=2)
+    finally = ""
+    
+    if num < 2
+        p phrase 
+    elsif num == 2
+        p phrase + " " + phrase
+    else     
+        finally = phrase
+        (num - 1).times do 
+            finally += " " + phrase 
+        end
+
+        p finally
+    end  
+end
+
+def start_of_word(string, index)
+    return string[0...index]
+end
+
+def first_word(sentence)
+    finally = sentence.split(" ")
+    return finally[0]
+end
+
+def titleize(string)
+    words = string.split(" ")
+    little_words = ["the", "over", "to", "on", "of", "for", "and"]
+
+    if words.length < 2
+        return string.capitalize 
+    else              
+        words.each_with_index do |word, i|
+            if !little_words.include?(word) || i == 0 
+            word.capitalize!
+            end    
+        end
+        return words.join(" ")
+    end   
+end   
+
