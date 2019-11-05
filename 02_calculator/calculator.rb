da1dff199463c1775b5f3fcf229a70473264e6e9
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-05 14:10:05 -0800

    added solutions for 02 -- did not attempt to write the rspec tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
-#write your code here
+def add(num1, num2)
+    return num1 + num2 
+end 
+
+def subtract(num1, num2)
+    return num1 - num2 
+end
+
+def sum(array)
+    if array.length == 0 
+        return 0 
+    else 
+        sum = 0
+        array.each do |i|
+            sum += i           
+        end
+        return sum 
+    end    
+end
+
