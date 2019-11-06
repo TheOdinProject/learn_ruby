9a831518ba7186f44b4f6be3cb510c2cba64a2de
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-06 08:51:29 -0800

    completed the book titles exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
-# write your code here
+
+    def initialize()
+        
+    end   
+
+    def title
+        @title
+    end 
+
+    def title=(string)
+        words = string.split(" ")
+        finally = [];
+        dont_capitalize = ["the", "a", "an", "in", "of", "and"]
+
+        words.each_with_index do |word, i|
+            if !dont_capitalize.include?(word) || (i == 0)
+                finally << word.capitalize
+            else
+                finally << word
+            end 
+        end   
+        
+        @title = finally.join(" ")
+    end
+
+
 end
