dc3c3207d7d319c3e079bd0e23cbc677ac0cc1ba
jerry <jerryhsu830@gmail.com>
2020-02-18 23:45:06 -0500

    problem 6: timer.rb

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :minutes, :hours
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+
+  def time_string
+    time_remaining = @seconds
+
+    @hours, time_remaining = time_remaining / 3600, time_remaining % 3600
+    @minutes, time_remaining = time_remaining / 60, time_remaining % 60
+    @seconds = time_remaining
+
+    "#{@hours.to_s.rjust(2,"0")}:#{@minutes.to_s.rjust(2,"0")}:#{@seconds.to_s.rjust(2,"0")}"
+  end
 end
