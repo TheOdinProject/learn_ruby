904b8bd91be509c4f3ea8244144818e21cd4f826
jerryhsu830@gmail.com <jerryhsu830@gmail.com>
2020-02-19 17:02:59 -0500

    exercise 3: simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
-#write your code here
+def echo(word)
+    word
+end
+
+def shout(word)
+    word.upcase
+end
+
+def repeat(word, num=2)
+    "#{(word +" ")* num}".rstrip
+end
+
+def start_of_word(word, num)
+    word.split("")[0...num].join("")
+end
+
+def first_word(word)
+    word.split(" ")[0]
+end
+
+def titleize(title)
+    little_words = ["and", "do", "the", "over"]
+
+    title.split(" ").each_with_index.map{ |word, idx| 
+        if idx == 0    
+            word.capitalize
+        elsif little_words.include?(word)
+            word
+        else
+            word.capitalize
+        end
+    }.join(" ")
+end
