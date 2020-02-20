d83edb59d8a75cf67059c29b42de4d7fca7b7e85
jerryhsu830@gmail.com <jerryhsu830@gmail.com>
2020-02-20 09:28:01 -0500

    exercise 4 & 5: pig_latin & book_titles

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
-#write your code here
+def translate(string)
+    string.split(" ").map { |word|
+        translate_word(word)
+    }.join(" ")
+end
+
+def translate_word(word)
+    vowels = "aeiou"
+    if vowels.include?(word[0])
+        "#{word}ay"
+    elsif word[0..2] == "sch"
+        translate_word( word[3..-1]+word[0..2] )
+    elsif word[0..1] == "qu"
+        translate_word( word[2..-1]+word[0..1] )
+    else
+        translate_word( word[1..-1]+word[0] )
+        # "#{word[1..-1]+word[0]}ay"
+    end
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+    attr_accessor :title, :lowercase_words
+    def lowercase_words
+        @lowercase_words = ["the", "a", "an", "and", "in", "of"]
+    end
+
+    def initialize(title)
+        @title = title.split(" ").each_with_index.map_with_index { |word, idx|
+            if idx == 0 || !@lowercase_words.include?(word)
+                word.capitalize
+            else
+                word
+            end
+        }
+
+        @title
+    end
 end
