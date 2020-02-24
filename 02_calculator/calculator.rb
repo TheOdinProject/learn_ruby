e83f4b9f125b0182382a07c7f0825baa3327c6ce
mark-ng <stngad@connect.ust.hk>
2020-02-24 21:37:11 +0800

    Finish the calculator and its spec

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(n1, n2)
+    n1 + n2
+end
+
+def subtract(n1, n2)
+    n1 - n2
+end
+
+def sum(arr)
+    arr.sum
+end
+
+def multiply(*others)
+    others.inject { |product, n| product * n } 
+end
+
+def power(n, exp)
+    ans = n
+    (exp-1).times do 
+        ans *= n
+    end
+    return ans
+end
+
+def factorial(n)
+    if n == 0
+        return 1
+    end
+
+    n * factorial(n-1)
+end
