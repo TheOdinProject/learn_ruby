416a91f1f12cd6d0c066daa8e021127c202cb816
mark-ng <stngad@connect.ust.hk>
2020-02-25 20:04:19 +0800

    Finish timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+    @time = Time.new(0)
+  end
+
+  def time_string
+    @time += @seconds
+    "#{padded(@time.hour)}:#{padded(@time.min)}:#{padded(@time.sec)}"
+  end
+
+  def padded(n)
+    if n.to_s.length > 1
+      return n.to_s
+    else
+      return '0' + n.to_s
+    end
+  end
 end
