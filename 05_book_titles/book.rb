74e56c08a17514b8d99689f5d19709450e0823dc
mark-ng <stngad@connect.ust.hk>
2020-02-25 19:44:45 +0800

    Finish book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def initialize
+        @title
+    end
+
+    def title
+        title_ls = @title.split(" ")
+        title_ls.map! do |ele|
+            if (ele != "and") and (ele != "in") and (ele != "the") and (ele != "of") and (ele != "a") and (ele != "an")
+                ele.capitalize
+            else
+                ele
+            end
+        end
+        title_ls[0] = title_ls[0].capitalize
+        title_ls.join(" ")
+    end
 end
