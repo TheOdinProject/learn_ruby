b2cb74d6f3878a6ad028902081d151a3f9207737
mark-ng <stngad@connect.ust.hk>
2020-02-25 19:31:03 +0800

    Finish pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
-#write your code here
+def translate(word)
+    ls = word.split(" ")
+    ls.map! do |ele|
+        translate_one_word(ele)
+    end
+    ls.join(" ")
+end
+
+def translate_one_word(word)
+    vowel = ["a","e","i","o","u"]
+
+    if vowel.include?(word[0].downcase)
+        return word + "ay"
+    elsif word[0..1] == 'qu'
+        translate(word[2..-1] + word[0..1])
+    else
+       new_word = word[1..-1] + word[0]
+       translate(new_word)
+    end
+end
