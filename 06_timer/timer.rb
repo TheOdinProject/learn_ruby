1e0b50c8635d2e0178ff8c8463a3dec398ec2b2f
reepz <a.wojtowicz@outlook.com>
2020-04-04 20:04:15 +0200

    finished all projects

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,33 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize(seconds = 0)
+    @seconds = seconds
+    
+    def padded(number)
+      if number < 10
+        "0#{number}"
+      else
+        "#{number}"
+      end
+    end
+
+    def time_string
+
+      if  seconds < 60
+        sec = seconds
+        "00:00:#{padded(seconds)}"
+      elsif seconds < 3600
+        min = seconds / 60
+        sec = seconds % 60
+        "00:#{padded(min)}:#{padded(sec)}"
+      else
+        hours = seconds / 3600
+        min = seconds % 3600 / 60
+        sec = seconds % 3600 % 60
+        "#{padded(hours)}:#{padded(min)}:#{padded(sec)}"
+      end
+
+    end
+  end
 end
